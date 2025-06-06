904f6c764e6a8be3943659a0ed8e566136fb5c27
Akshay Gurav <mathsjourneywithakshay@gmail.com>
2025-06-06 23:30:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def get_pages():
     menu_order = [
         ('MRIxAI', '1_MRIxAI.py', 'https://nexo-mri3d.onrender.com/'),
         ('Genethink AI', '2_Genethink AI.py', 'https://nexo-1.onrender.com/'),
-        ('NexoGPT', '3_NexoGPT.py', 'https://nexo-1.onrender.com/'),
-        ('NewsNX', '4_NewsNX.py', 'https://nexo-1-3szu.onrender.com/'),
+        ('NexoGPT', '3_NexoGPT.py', 'https://nexo-1-3szu.onrender.com/'),
+        ('NewsNX', '4_NewsNX.py', 'https://nexo-2.onrender.com/'),
         ('About', '5_About.py', 'https://nexo-about.onrender.com/'),
         ('Demo', 'demo', 'https://nexo-demo.onrender.com/'),  # Placeholder URL; update if needed
     ]
